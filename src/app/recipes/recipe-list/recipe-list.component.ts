d6f68bad1e018e226a0e0714e2cfc4188670cfb1
freeman29 <tordafrancis@gmail.com>
2017-04-19 21:18:20 +0800

    feat(recipes): add recipe-list component UI

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-
+	recipes: Recipe[] = [
+		new Recipe(
+			'Pork Humba', 
+			'Humba (HOOM-BAA) is a Filipino dish similar to adobo.', 
+			'http://i0.wp.com/russianfilipinokitchen.files.wordpress.com/2014/07/dsc_0628.jpg?ssl=1'
+		),
+		new Recipe(
+			'Bicol Express', 
+			'Pork cooked in coconut milk with shrimp paste and chilies.', 
+			'http://homekitchenomics.com/wp-content/uploads/2014/03/bicol_express_recipe.jpg'
+		),
+		new Recipe(
+			'Dinengdeng', 
+			'Vegetable dish that originated from the Ilocos region in the Philippines.', 
+			'https://s-media-cache-ak0.pinimg.com/736x/fa/95/3b/fa953b2c54d80525d04c47c54610b992.jpg'
+		)
+	];
+	
   constructor() { }
 
   ngOnInit() {
